5d3d14255f236c8050fb5f536941ece05d4f6b4c
abhishekshilu978 <abhishekkumar1087@gmail.com>
2025-11-11 15:29:01 +0530

    Abhishek

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [sortData,setSortData]=useState([])
+ 
+
+  const tableData=
+[
+
+    { date: "2022-09-01", views: 100, article: "Article 1" },
+    { date: "2023-09-01", views: 100, article: "Article 1" },
+    { date: "2023-09-02", views: 150, article: "Article 2" },
+    { date: "2023-09-02", views: 120, article: "Article 3" },
+    { date: "2020-09-03", views: 200, article: "Article 4" }
+
+]
+
+useEffect(()=>{
+  setSortData(tableData)
+},[])
+
+const sortByDate=()=>{
+    let sortedDate=tableData.sort((a,b)=>new Date(a.date)-new Date(b.date));
+    console.log(sortedDate)
+    setSortData(sortedDate)
+}
+const sortByViews=()=>{
+   let sortedDate=tableData.sort((a,b)=>a.views-b.views);
+    console.log(sortedDate)
+    setSortData(sortedDate)
+}
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button className='btn' onClick={sortByDate}>Sort by Date</button>
+        <button className='btn' onClick={sortByViews}>Sort By Views</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <table>
+        <thead>
+          <td>Date</td>
+          <td>Views</td>
+          <td>Article</td>
+        </thead>
+        <tbody>
+         
+          {sortData.map((data)=>
+           <tr>
+              <td>{data.date}</td>
+              <td>{data.views}</td>
+              <td>{data.article}</td>
+          </tr>
+          
+          )}
+          
+        </tbody>
+      </table>
     </>
   )
 }
 
-export default App
+export default App;
